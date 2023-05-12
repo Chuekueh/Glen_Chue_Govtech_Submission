1bc368d275faf7f6d4fbef136926374f47905300
Glen <gchue76@gmail.com>
2023-05-13 01:40:20 +0800

    enforce data types when loading

---
+++ Section_1/Data_Engineering_Scripts/load.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+from constants import *
+from utils import enforce_data_types
+
+succesful_applicants = pd.read_csv(f"{TRANSFORM_DIR}/{SUCESS_TRANSFORM_FILE}")
+unsuccesful_applicants = pd.read_csv(f"{TRANSFORM_DIR}/{UNSUCESSFUL_TRANSFORM_FILE}")
+
+column_data_types = {
+    'membership_id':'str',
+    'first_name':'str',
+    'last_name': 'str',
+    'email': 'str',
+    'date_of_birth': 'str',
+    'mobile_no': 'str',
+    'above_18': 'bool'
+}
+
+succesful_applicants = enforce_data_types(succesful_applicants, column_data_types)
+unsuccesful_applicants = enforce_data_types(unsuccesful_applicants, column_data_types)
+
+print('done')
