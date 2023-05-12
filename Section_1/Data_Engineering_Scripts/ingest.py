c156ea9e1495d1fad49e0eaa2dfa05289fb7909c
Glen <gchue76@gmail.com>
2023-05-13 01:54:10 +0800

    wrap ingestion step around a function

--- Section_1/Data_Engineering_Scripts/ingest.py
+++ Section_1/Data_Engineering_Scripts/ingest.py
@@ -4,16 +4,24 @@ from datetime import datetime
 from constants import *
 from utils import add_in_cols
 
-#Loading in Input Data 
-input_data = pd.read_csv(f'{INPUT_DIR}/applications_dataset_1.csv')
+def data_ingestion ():
+    #Loading in Input Data 
+    input_data = pd.read_csv(f'{INPUT_DIR}/applications_dataset_1.csv')
 
-#Creating Additional Column for preprocessing 
-processed_input_data = add_in_cols(input_data)
+    #Creating Additional Column for preprocessing 
+    processed_input_data = add_in_cols(input_data)
 
-#Standardise Order of Columns
-ingested_data = processed_input_data[['membership_id','first_name','last_name','name','email','date_of_birth',
-                            'mobile_no','above_18','is_succesful',]]
+    #Standardise Order of Columns
+    ingested_data = processed_input_data[['membership_id','first_name','last_name','name','email','date_of_birth',
+                                'mobile_no','above_18','is_succesful',]]
 
-ingested_data.to_csv(f"{INGEST_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", index=False)
+    ingested_data.to_csv(f"{INGEST_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", index=False)
 
-print(f"{len(ingested_data)} data points sucessfully ingested at {datetime.now().strftime('%Y/%m/%d %H')} hrs")
+    print(f"{len(ingested_data)} data points sucessfully ingested at {datetime.now().strftime('%Y/%m/%d %H')} hrs")
+
+if __name__ == "__main__":
+    try:
+        data_ingestion ()
+    except Exception as e: 
+        print ("WARNING Data not successfully ingested")
+        print(e)
