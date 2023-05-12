ca66ab5ee872758f598bbeafe519cf0edd2de511
Glen <gchue76@gmail.com>
2023-05-12 23:51:57 +0800

    add in ingest and transform file constants

--- Section_1/Data_Engineering_Scripts/constants.py
+++ Section_1/Data_Engineering_Scripts/constants.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 INPUT_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Input_Data'
 INGEST_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Ingested_Data'
 LOAD_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Load_data'
 TRANSFORM_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Transformed_data'
 
 
-INGEST_FILE = 
+INGEST_FILE = f"data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv"
+TRANSFORM_FILE = f"transformed_data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv"
