8b290d0357ab95758c7a802f0cc47dd19b44a614
Glen <gchue76@gmail.com>
2023-05-13 02:31:48 +0800

    Refactor extraction to handle failures

--- Section_1/DE_Pipeline_Scripts/extract.py
+++ Section_1/DE_Pipeline_Scripts/extract.py
@@ -15,28 +15,23 @@ def data_ingestion ():
         try:
             data = pd.read_csv(file)
             input_data = pd.concat([input_data,data], ignore_index=True)
-        except Exception as e:
-            continue 
 
+            #Creating Additional Column for preprocessing 
+            processed_input_data = add_in_cols(input_data)
 
-    #Creating Additional Column for preprocessing 
-    processed_input_data = add_in_cols(input_data)
+            #Standardise Order of Columns
+            ingested_data = processed_input_data[['membership_id','first_name','last_name','name','email','date_of_birth',
+                                        'mobile_no','above_18','is_succesful']]
 
-    #Standardise Order of Columns
-    ingested_data = processed_input_data[['membership_id','first_name','last_name','name','email','date_of_birth',
-                                'mobile_no','above_18','is_succesful']]
+            ingested_data.to_csv(f"{EXTRACT_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", index=False)
 
-    ingested_data.to_csv(f"{EXTRACT_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", index=False)
+            print(f"{len(ingested_data)} data points sucessfully ingested at {datetime.now().strftime('%Y/%m/%d %H')} hrs")
 
-    print(f"{len(ingested_data)} data points sucessfully ingested at {datetime.now().strftime('%Y/%m/%d %H')} hrs")
+            os.remove(file)
+            print(f"Removed {file} as successfully extracted")
 
-    for file in files:
-        os.remove(file)
-        print(f"Removed {file} as successfully extracted")
+        except Exception as e: 
+            print(e)
 
 if __name__ == "__main__":
-    try:
-        data_ingestion ()
-    except Exception as e: 
-        print ("WARNING Data not successfully ingested")
-        print(e)
+    data_ingestion ()
