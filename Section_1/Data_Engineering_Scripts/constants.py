7c0d8be85009d8695d18725a519d3959889451e8
Glen <gchue76@gmail.com>
2023-05-13 02:00:35 +0800

    minor refactor

--- Section_1/Data_Engineering_Scripts/constants.py
+++ Section_1/Data_Engineering_Scripts/constants.py
@@ -3,8 +3,8 @@ from datetime import datetime
 INPUT_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Input_Data'
 EXTRACT_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Extacted_Data'
 TRANSFORM_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Transformed_data'
-SUCCESFUL_LOAD_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Load_Successful_Applications'
-UNSUCCESFUL_LOAD_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Load_Unuccessful_Applications'
+SUCCESFUL_LOAD_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Successful_Applications'
+UNSUCCESFUL_LOAD_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Unuccessful_Applications'
 
 EXTRACT_FILE = f"data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv"
 SUCESS_TRANSFORM_FILE = f"transformed_data_batch_succesful_{datetime.now().strftime('%Y%m%d%H')}.csv"
