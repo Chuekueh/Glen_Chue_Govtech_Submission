e84678923c006157dd6f01a902059ca00810ed76
Glen <gchue76@gmail.com>
2023-05-12 23:39:27 +0800

    Add in Constants Folder for commonly used variable

---
+++ Section_1/Data_Engineering_Scripts/constants.py
@@ -0,0 +1,4 @@
+INPUT_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Input_Data'
+INGEST_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Ingested_Data'
+LOAD_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Load_data'
+TRANSFORM_DIR = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Transformed_data'
--- Section_1/Data_Engineering_Scripts/ingest.py
+++ Section_1/Data_Engineering_Scripts/ingest.py
@@ -1,9 +1,10 @@
 import pandas as pd 
 import numpy as np
 from datetime import datetime
+from constants import *
 
 #Loading in Input Data 
-input_data = pd.read_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/Input Data/applications_dataset_1.csv')
+input_data = pd.read_csv(f'{INPUT_DIR}/applications_dataset_1.csv')
 
 #Creating Additional Column for preprocessing 
 input_data['first_name'] = ""
@@ -17,5 +18,6 @@ input_data['processed_time'] = datetime.now().strftime('%y%m%d%H')
 ingested_data = input_data[['membership_id','first_name','last_name','name','email','date_of_birth',
                             'mobile_no','above_18','succesful','processed_time']]
 
-ingested_data.to_csv(f"/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_1/DE_Pipeline_Data/Ingested_Data/data_batch_{datetime.now().strftime('%Y%m%d%H')}")
-print("Done")
+ingested_data.to_csv(f"{INGEST_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}")
+
+print(f"{len(ingested_data)} data points sucessfully ingested at {datetime.now().strftime('%Y %m %d %H')} hrs")
