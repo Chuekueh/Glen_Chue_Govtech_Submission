a3c991a31645923f65b84c85265553f023fea22e
Glen <gchue76@gmail.com>
2023-05-13 01:48:36 +0800

    Add in log messages for loading script

--- Section_1/Data_Engineering_Scripts/load.py
+++ Section_1/Data_Engineering_Scripts/load.py
@@ -2,9 +2,11 @@ import pandas as pd
 from constants import *
 from utils import enforce_data_types
 
+print(f"Preparing data from {TRANSFORM_DIR}/{SUCESS_TRANSFORM_FILE} and {TRANSFORM_DIR}/{UNSUCESSFUL_TRANSFORM_FILE} for Loading")
 succesful_applicants = pd.read_csv(f"{TRANSFORM_DIR}/{SUCESS_TRANSFORM_FILE}")
 unsuccesful_applicants = pd.read_csv(f"{TRANSFORM_DIR}/{UNSUCESSFUL_TRANSFORM_FILE}")
 
+print("Enforcing Datatypes")
 column_data_types = {
     'membership_id':'str',
     'first_name':'str',
@@ -18,7 +20,9 @@ column_data_types = {
 succesful_applicants = enforce_data_types(succesful_applicants, column_data_types)
 unsuccesful_applicants = enforce_data_types(unsuccesful_applicants, column_data_types)
 
+print(f"Saving cleaned succesful applicants data to {SUCCESFUL_LOAD_DIR}/{SUCESS_LOAD_FILE}")
 succesful_applicants.to_csv(f"{SUCCESFUL_LOAD_DIR}/{SUCESS_LOAD_FILE}")
+print(f"Saving cleaned succesful applicants data to {UNSUCCESFUL_LOAD_DIR}/{UNSUCESSFUL_LOAD_FILE}")
 unsuccesful_applicants.to_csv(f"{UNSUCCESFUL_LOAD_DIR}/{UNSUCESSFUL_LOAD_FILE}")
 
 print('done')
