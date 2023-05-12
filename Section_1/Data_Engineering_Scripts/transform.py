b9e614a5d909b2971baa129d10347fbc3b34a4b6
Glen <gchue76@gmail.com>
2023-05-13 01:28:35 +0800

    add in log messages for transform script

--- Section_1/Data_Engineering_Scripts/transform.py
+++ Section_1/Data_Engineering_Scripts/transform.py
@@ -7,24 +7,38 @@ print(f"Beginning Data Transformation for {INGEST_DIR}/{INGEST_FILE}")
 raw_data = pd.read_csv(f'{INGEST_DIR}/{INGEST_FILE}')
 
 # Preprocessing Steps
-print(f"Beginning Date_of_birth Column Transformation")
+print(f"Beginning date_of_birth Column Transformation")
 raw_data['date_of_birth'] = raw_data['date_of_birth'].apply(process_dob)
+
+print(f"Beginning above_18 Column Transformation")
 raw_data['above_18'] = raw_data['date_of_birth'].apply(is_18_years_old)
+
+print(f"Beginning succesful Column Transformation")
 raw_data['succesful'] = raw_data.apply(succesful_application, axis=1)
+
+print(f"Beginning first_name Column Transformation")
 raw_data['first_name'] = raw_data['email'].apply(lambda x:x.split('@')[0].split('_')[0])
+
+print(f"Beginning last_name Column Transformation")
 raw_data['last_name'] = raw_data['email'].apply(lambda x:x.split('@')[0].split('_')[1])
 
 # Split into Successful and Unsuccesful appplicants 
+print(f"Splitting Dataset into Succesful and Unsuccesful applicants")
 succesful_applicants = raw_data[raw_data['succesful'] == True][['membership_id','first_name','last_name','email','date_of_birth',
                             'mobile_no','above_18']]
+print(f"There are a total of {len(succesful_applicants)} succesful applicants in this batch")
 unsuccesful_applicants = raw_data[raw_data['succesful'] == False][['membership_id','first_name','last_name','email','date_of_birth',
                             'mobile_no','above_18']]
+print(f"There are a total of {len(unsuccesful_applicants)} unsuccesful applicants in this batch")
 
 # Generate Membership ID for succesful applicants only
+print(f"Generating Membership ID for Succesful Applicants")
 succesful_applicants['membership_id'] = succesful_applicants.apply(generate_membership_id, axis=1)
 
 # Write to relevant folders
+print(f"Saving Succesful applicants to {TRANSFORM_DIR}/{SUCESS_TRANSFORM_FILE}")
 succesful_applicants.to_csv(f"{TRANSFORM_DIR}/{SUCESS_TRANSFORM_FILE}")
+print(f"Saving Unuccesful applicants to {TRANSFORM_DIR}/{UNSUCESSFUL_TRANSFORM_FILE}")
 unsuccesful_applicants.to_csv(f"{TRANSFORM_DIR}/{UNSUCESSFUL_TRANSFORM_FILE}")
 
 print('Done')
--- Section_1/Data_Engineering_Scripts/utils.py
+++ Section_1/Data_Engineering_Scripts/utils.py
@@ -17,7 +17,7 @@ def process_dob(date_str):
     try:
         return parser.parse(date_str).strftime('%Y%m%d')
     except ParserError as e:
-        print(e)
+        print(f'ERROR for dob value {date_str}:', e) 
         return pd.NaT
     
 def is_18_years_old(date_str):
@@ -26,7 +26,7 @@ def is_18_years_old(date_str):
         eighteen_years_ago = pd.Timestamp.now() - relativedelta(years=18)
         return date_of_birth <= eighteen_years_ago
     except TypeError as e:
-        print(e) 
+        print(f'ERROR for dob value {date_str}:', e) 
         return False
 
 def succesful_application(row):
