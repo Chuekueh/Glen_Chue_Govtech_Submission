52646ba836357960286908e837c2bb3753517bef
Glen <gchue76@gmail.com>
2023-05-13 00:58:56 +0800

    removed processed time for ingest script

--- Section_1/Data_Engineering_Scripts/transform.py
+++ Section_1/Data_Engineering_Scripts/transform.py
@@ -5,13 +5,16 @@ from utils import process_dob, is_18_years_old, succesful_application
 # obtain ingested data that was processed within the same hour 
 raw_data = pd.read_csv(f'{INGEST_DIR}/{INGEST_FILE}')
 
-# Standardise Datetime format
-
+# Preprocessing Steps
 raw_data['date_of_birth'] = raw_data['date_of_birth'].apply(process_dob)
 raw_data['above_18'] = raw_data['date_of_birth'].apply(is_18_years_old)
 raw_data['succesful'] = raw_data.apply(succesful_application, axis=1)
 raw_data['first_name'] = raw_data['email'].apply(lambda x:x.split('@')[0].split('_')[0])
 raw_data['last_name'] = raw_data['email'].apply(lambda x:x.split('@')[0].split('_')[1])
+
+#Split into Successful and Unsuccesful appplicants 
+succesful_applicants = raw_data[['membership_id','first_name','last_name','email','date_of_birth',
+                            'mobile_no','above_18','succesful','processed_time']]
 
 print('Done')
 
