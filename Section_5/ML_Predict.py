4cec4310516709deeb45d04904fa2e7cac1001fc
Glen <gchue76@gmail.com>
2023-05-13 17:57:13 +0800

    create DF to choose best model from

--- Section_5/ML_Predict.py
+++ Section_5/ML_Predict.py
@@ -1,7 +1,11 @@
 import pandas as pd 
 import json 
 
-model_metadata = json.loads('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/model_results.json')
+# Read the contents of the JSON file
+with open('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/model_results.json') as file:
+    json_data = file.read()
+
+model_metadata = json.loads(json_data)
 
 # converting json dataset from dictionary to dataframe
 available_models = pd.DataFrame.from_dict(model_metadata, orient='index')
