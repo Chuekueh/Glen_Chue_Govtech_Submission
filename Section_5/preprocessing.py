381e393c2078104ba045dfd6a13d273f7850c709
Glen <gchue76@gmail.com>
2023-05-13 15:28:14 +0800

    edit train test split

--- Section_5/preprocessing.py
+++ Section_5/preprocessing.py
@@ -16,10 +16,10 @@ non_dummy = car_data
 non_dummy.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/Data/non_dummy.csv', header=True)
 
 #Splitting Data into training and testing Datasets without stratifying based on class 
-X = non_dummy.drop('buying',axis=1)
+X = non_dummy.drop(['buying','row_num'],axis=1)
 y = non_dummy['buying']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Print the buying distribution in the train and test sets
 print("Buying distribution in train set:")
@@ -27,13 +27,32 @@ print(y_train.value_counts(normalize=True))
 print("\nBuying distribution in test set:")
 print(y_test.value_counts(normalize=True))
 
+X_train.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_80_20/train_features.csv')
+y_train.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_80_20/train_label.csv')
+X_test.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_80_20/test_features.csv')
+y_test.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_80_20/test_label.csv')
+
+
+#Splitting Data into training and testing Datasets with stratifying based on class
+#Splitting Data into training and testing Datasets without stratifying based on class 
+X = non_dummy.drop(['class','row_num'],axis=1)
+y = non_dummy['class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y, random_state=42)
+
+# Print the buying distribution in the train and test sets
+print("Class distribution in train set:")
+print(y_train.value_counts(normalize=True))
+print("buying distribution in train set:")
+print(X_train['buying'].value_counts(normalize=True))
+print("\nClass distribution in test set:")
+print(y_test.value_counts(normalize=True))
+print("buying distribution in test set:")
+print(X_test['buying'].value_counts(normalize=True))
+
 X_train.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_75_25/train_features.csv')
 y_train.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_75_25/train_label.csv')
 X_test.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_75_25/test_features.csv')
 y_test.to_csv('/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data/Non_Stratified_75_25/test_label.csv')
-
-
-#Splitting Data into training and testing Datasets with stratifying based on class
-
 print('done')
 
