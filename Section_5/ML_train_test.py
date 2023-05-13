94e5a420327e4b32d6b96f47f309574e54b85704
Glen <gchue76@gmail.com>
2023-05-13 17:06:52 +0800

    define test model function

--- Section_5/ML_train_test.py
+++ Section_5/ML_train_test.py
@@ -2,7 +2,7 @@ import os
 from ML_func import * 
 import pandas as pd
 
-def train_models(dir_path, X_train_path, y_train_path, label_encoder_path, encoder_path):
+def train_models(dir_path, X_train_path, y_train_path, label_encoder_path, encoder_path, model_path):
         # Assuming you have the X_train and y_train data for each directory
         
         X_train = pd.read_csv(X_train_path, index_col=0)
@@ -14,16 +14,42 @@ def train_models(dir_path, X_train_path, y_train_path, label_encoder_path, encod
         best_rf_model, best_params = find_best_random_forest(X_train_encoded, y_train_encoded, dir_path)
         
         # Save the best model to the same directory
-        model_save_path = os.path.join(dir_path, "best_rf_model.pkl")
-        with open(model_save_path, 'wb') as file:
+        with open(model_path, 'wb') as file:
             pickle.dump(best_rf_model, file)
         
         print(f"Best model saved for directory: {dir_name}")
         print("Best Parameters:", best_params)
      
+def test_models(dir_path, X_test_path, y_test_path, label_encoder_path, encoder_path, model_path):
+     
+    X_test = pd.read_csv(X_test_path, index_col=0)
+    y_test = pd.read_csv(y_test_path, index_col=0)
 
+    with open(encoder_path, 'rb') as file:
+        encoder = pickle.load(file)
+    
+    with open(label_encoder_path, 'rb') as file:
+        label_encoder = pickle.load(file)
+    
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    
+    # Encode the test data using the loaded encoders
+    X_test_encoded = encoder.transform(X_test)
+    y_test_encoded = label_encoder.transform(y_test)
+    
+    # Make predictions on the test set
+    y_pred = model.predict(X_test_encoded)
+    
+    # Calculate precision, recall, and F1 score
+    precision = precision_score(y_test_encoded, y_pred)
+    recall = recall_score(y_test_encoded, y_pred)
+    f1 = f1_score(y_test_encoded, y_pred)
+    
+    return precision, recall, f1
 
 if __name__ == "__main__":
+
     data_dir = '/Users/glen/Desktop/Glen_Chue_Govtech_Submission/Section_5/ML_Data'
     for dir_name in os.listdir(data_dir):
         dir_path = os.path.join(data_dir, dir_name)
@@ -35,9 +61,12 @@ if __name__ == "__main__":
 
             label_encoder_path = os.path.join(dir_path, "label_encoder.pkl")
             encoder_path = os.path.join(dir_path, "encoder.pkl")
+            model_path = os.path.join(dir_path, "best_rf_model.pkl")
+
+            train_models(dir_path, X_train_path, y_train_path, label_encoder_path, encoder_path, model_path)
+            test_models(dir_path, X_train_path, y_train_path, label_encoder_path, encoder_path, model_path)
 
 
-            train_models(dir_path, X_train_path, y_train_path, label_encoder_path, encoder_path)
 
 
 
