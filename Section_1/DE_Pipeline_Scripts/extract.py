47842a69fe6141a77388566e5a37ba8dcaeb2345
Glen <gchue76@gmail.com>
2023-05-13 23:49:42 +0800

    Edit logic for extraction of data

--- Section_1/DE_Pipeline_Scripts/extract.py
+++ Section_1/DE_Pipeline_Scripts/extract.py
@@ -17,25 +17,26 @@ def data_extraction ():
             data = pd.read_csv(file)
             input_data = pd.concat([input_data,data], ignore_index=True)
 
-            #Creating Additional Column for preprocessing 
-            processed_input_data = add_in_cols(input_data)
-
-            #Standardise Order of Columns
-            ingested_data = processed_input_data[['membership_id','first_name','last_name','name','email','date_of_birth',
-                                        'mobile_no','above_18','is_succesful']]
-
-            ingested_data.to_csv(f"{EXTRACT_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", index=False)
-
-            print(f"{len(ingested_data)} data points sucessfully ingested at {datetime.now().strftime('%Y/%m/%d %H')} hrs")
-
-            shutil.copy(f"{EXTRACT_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", ARCHIVE_DIR)
-            print(f"data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv moved to {ARCHIVE_DIR} as successfully extracted")
-
-            os.remove(file)
-            print(f"{file} remove since succesfully extracted")
-
         except Exception as e: 
             print(e)
+
+    #Creating Additional Column for preprocessing 
+    processed_input_data = add_in_cols(input_data)
+
+    #Standardise Order of Columns
+    ingested_data = processed_input_data[['membership_id','first_name','last_name','name','email','date_of_birth',
+                                'mobile_no','above_18','is_succesful']]
+
+    ingested_data.to_csv(f"{EXTRACT_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", index=False)
+
+    print(f"{len(ingested_data)} data points sucessfully ingested at {datetime.now().strftime('%Y/%m/%d %H')} hrs")
+
+    shutil.copy(f"{EXTRACT_DIR}/data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv", ARCHIVE_DIR)
+    print(f"data_batch_{datetime.now().strftime('%Y%m%d%H')}.csv moved to {ARCHIVE_DIR} as successfully extracted")
+
+    os.remove(file)
+    print(f"{file} remove since succesfully extracted")
+
 
 if __name__ == "__main__":
     data_extraction ()
