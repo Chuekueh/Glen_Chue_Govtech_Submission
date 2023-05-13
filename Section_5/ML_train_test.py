eb3b4fdeaa7e4d21540f31e78a357192f8176910
Glen <gchue76@gmail.com>
2023-05-13 17:27:54 +0800

    add in best score into output

--- Section_5/ML_train_test.py
+++ Section_5/ML_train_test.py
@@ -11,7 +11,7 @@ def train_models(dir_path, X_train_path, y_train_path, label_encoder_path, encod
         X_train_encoded, y_train_encoded = encode_categorical(X_train, y_train, label_encoder_path, encoder_path)
 
         # Find the best Random Forest model
-        best_rf_model, best_params = find_best_random_forest(X_train_encoded, y_train_encoded, dir_path)
+        best_rf_model, best_params, best_score = find_best_random_forest(X_train_encoded, y_train_encoded, dir_path)
         
         # Save the best model to the same directory
         with open(model_path, 'wb') as file:
@@ -19,8 +19,9 @@ def train_models(dir_path, X_train_path, y_train_path, label_encoder_path, encod
         
         print(f"Best model saved for directory: {dir_name}")
         print("Best Parameters:", best_params)
+        print("Best Score:", best_score)
      
-def test_models(dir_path, X_test_path, y_test_path, label_encoder_path, encoder_path, model_path):
+def test_models(dir_path, X_test_path, y_test_path, label_encoder_path, encoder_path, model_path, average='weighted'):
      
     X_test = pd.read_csv(X_test_path, index_col=0)
     y_test = pd.read_csv(y_test_path, index_col=0)
@@ -42,9 +43,9 @@ def test_models(dir_path, X_test_path, y_test_path, label_encoder_path, encoder_
     y_pred = model.predict(X_test_encoded)
     
     # Calculate precision, recall, and F1 score
-    precision = precision_score(y_test_encoded, y_pred)
-    recall = recall_score(y_test_encoded, y_pred)
-    f1 = f1_score(y_test_encoded, y_pred)
+    precision = precision_score(y_test_encoded, y_pred, average=average)
+    recall = recall_score(y_test_encoded, y_pred, average=average)
+    f1 = f1_score(y_test_encoded, y_pred, average=average)
     
     return precision, recall, f1
 
