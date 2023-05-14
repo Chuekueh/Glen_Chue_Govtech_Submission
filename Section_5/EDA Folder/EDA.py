c7f43960469ba09c2d26805b31baecbcb4626a49
Glen <gchue76@gmail.com>
2023-05-14 19:05:28 +0800

    assess chi-square association between variables

--- Section_5/EDA Folder/EDA.py
+++ Section_5/EDA Folder/EDA.py
@@ -27,3 +27,32 @@ vgood_class = non_dummy[non_dummy['class'] == 'vgood']
 print(vgood_class['buying'].value_counts())
 vgood_class_value_counts = vgood_class.apply(lambda x: x.value_counts()).T.stack()
 print("Done")
+
+
+# Looking at association between variables 
+# # Create an empty DataFrame to store the results
+results_df = pd.DataFrame(columns=['Variable 1', 'Variable 2', 'Chi-square', 'P-value', 'Degrees of Freedom'])
+column_names = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+# Loop through all pairs of variables
+for i in range(len(column_names)):
+    for j in range(i+1, len(column_names)):
+        variable1 = column_names[i]
+        variable2 = column_names[j]
+
+        # Create a contingency table
+        contingency_table = pd.crosstab(data[variable1], data[variable2])
+
+        # Perform the chi-square test
+        chi2, p, dof, expected = chi2_contingency(contingency_table)
+
+        # Store the results in the DataFrame
+        results_df = results_df.append({
+            'Variable 1': variable1,
+            'Variable 2': variable2,
+            'Chi-square': chi2,
+            'P-value': p,
+            'Degrees of Freedom': dof
+        }, ignore_index=True)
+
+# Save the results to a CSV file
+results_df.to_csv('association_results.csv', index=False)
